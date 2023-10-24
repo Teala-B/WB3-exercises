47d163e36cae12bcbca562e1a05c637e540448cb
Teala-B <bradleyteala@gmail.com>
2023-10-24 15:34:13 -0500

    added and did simple functions

---
+++ Functions/simple_functions.js
@@ -0,0 +1,20 @@
+
+function favoriteThings() {
+    console.log("Name: Tea'La");
+    console.log("Show: Charmed");
+    console.log("Musician/Band: N/A");
+}
+
+favoriteThings()
+
+function whyImHere() {
+    console.log("I am here because I would like to have a career in coding.");
+}
+
+whyImHere()
+
+function favoritePlaces() {
+    console.log("My favorite place to visit is back home so that I can see my extended family.");
+}
+
+favoritePlaces()
